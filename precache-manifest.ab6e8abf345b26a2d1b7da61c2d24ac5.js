59727cf84f0344aecbb0397d74405cae9469ad70
Alexandra Dima <alexandra.dima89@yahoo.com>
2020-11-10 17:32:36 +0100

    Updates

---
+++ precache-manifest.ab6e8abf345b26a2d1b7da61c2d24ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64113da890b2abaf5c270d1b3f67fe03",
+    "url": "/fashion-landing-page/index.html"
+  },
+  {
+    "revision": "c9c52e3ecf6a680cada1",
+    "url": "/fashion-landing-page/static/css/main.68d2d4a6.chunk.css"
+  },
+  {
+    "revision": "38db58aff7704a6101d2",
+    "url": "/fashion-landing-page/static/js/2.3bfbefdb.chunk.js"
+  },
+  {
+    "revision": "18dc01920e9d700949903399ed2349ab",
+    "url": "/fashion-landing-page/static/js/2.3bfbefdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c52e3ecf6a680cada1",
+    "url": "/fashion-landing-page/static/js/main.0012d035.chunk.js"
+  },
+  {
+    "revision": "b36d1b6afec5f8032c75",
+    "url": "/fashion-landing-page/static/js/runtime-main.3b5080de.js"
+  }
+]);
